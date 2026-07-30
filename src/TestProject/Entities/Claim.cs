79a2df9081181cb03c35232be5e32aafdb3f11c1
Danny Goodacre <danny.goodacre@bath.edu>
2026-07-30 20:33:08 +0100

    WIP

--- src/TestProject/Entities/Claim.cs
+++
@@ -1,8 +0,0 @@
-namespace TestProject;
-
-public class Claim
-{
-    public int Id { get; set; }
-
-    public string Name { get; set; } = null!;
-}
--- src/TestProject/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-namespace TestProject;
-
-public class User
-{
-    public int Id { get; set; }
-
-    public string Name { get; set; } = null!;
-}
