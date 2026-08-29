a449e69d284d0e0bcc542847d82466c0c60bf09d
Danny Goodacre <danny.goodacre@bath.edu>
2026-08-29 17:32:44 +0100

    WIP

--- src/DannyGoodacre.Functional.Test/Program.cs
+++ src/DannyGoodacre.Functional.Test/Program.cs
@@ -6,16 +6,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Result<int> result = Test();
+        IResult<int> res1 = Result.Success(123);
 
-        if (result is DomainError<int> domainError)
-        {
-            Console.WriteLine(domainError.Error);
-        }
+        IResult<int> result = Test();
+
+        IResult<int> res2 = Result.Canceled<int>();
+
+        Console.WriteLine(res2);
+
+        IResult<string> res3 = res2.MapFailure<string>();
+
+        Console.WriteLine(res3);
+
+        IResult res4 = res2;
+
+        Console.WriteLine(res4);
+
+        IResult res5 = Result.Canceled<int>();
+
+        // switch (result)
+        // {
+        //     case Success<int> success:
+        //         Console.WriteLine(success.Value);
+        //         break;
+        //
+        //     case DomainError domainError:
+        //         Console.WriteLine(domainError.Error);
+        //         break;
+        // }
     }
 
-    static Result<int> Test()
+    static IResult<int> Test()
     {
-        return new DomainError<int>("Error message");
+        // return Result.Success(123);
+        return Result.DomainError<int>("foo");
     }
 }
--- src/DannyGoodacre.Primitives/ResultExtensions.cs
+++ src/DannyGoodacre.Primitives/ResultExtensions.cs
@@ -1,28 +1,27 @@
-// namespace DannyGoodacre.Primitives;
-//
-// public static class ResultExtensions
-// {
-//     extension<T>(Result<T> result)
-//     {
-//         public Result<TOut> Bind<TOut>(Func<T, Result<TOut>> f)
-//             => result.IsSuccess
-//                 ? f(result.Value)
-//                 : result.MapFailure<TOut>();
-//
-//         public Result<TOut> Map<TOut>(Func<T, TOut> f)
-//             => result.IsSuccess
-//                 ? Result<TOut>.Success(f(result.Value))
-//                 : result.MapFailure<TOut>();
-//
-//         public TOut Match<TOut>(Func<T, TOut> onSuccess, Func<Status, Error?, TOut> onFailure)
-//             => result.IsSuccess
-//                 ? onSuccess(result.Value)
-//                 : onFailure(result.Status, result.Error);
-//     }
-//
-//     extension(Result)
-//     {
-//         public static Result<T> Unit<T>(T value)
-//             => Result.Success(value);
-//     }
-// }
+namespace DannyGoodacre.Primitives;
+
+public static class ResultExtensions
+{
+    extension<TOut>(IResult result)
+    {
+        public IResult<TOut> MapFailure()
+            => result switch
+            {
+                Canceled => new Canceled<TOut>(),
+
+                Conflict conflict => new Conflict<TOut>(conflict.Error),
+
+                DomainError domainError => new DomainError<TOut>(domainError.Error),
+
+                InternalError internalError => new InternalError<TOut>(internalError.Error),
+
+                Invalid invalid => new Invalid<TOut>(invalid.ValidationState),
+
+                NotFound => new NotFound<TOut>(),
+
+                Success => throw new InvalidOperationException("Cannot map a successful result to a failure."),
+
+                _ => throw new ArgumentOutOfRangeException(nameof(result))
+            };
+    }
+}
